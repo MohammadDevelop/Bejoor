c0da28dbb03b2ffb1a13f3192335bd50d7664102
mohamad.develop@gmail.com <mohamad.develop@gmail.com>
2024-09-12 15:45:50 +0330

    document init

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,3 @@
-import sphinx_rtd_theme
-
-html_theme = 'sphinx_rtd_theme'
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 project = 'Bejoor'
 copyright = '2024, Mohammad Mahmoodi Varnamkhasti'
