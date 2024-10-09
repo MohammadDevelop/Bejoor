cdd606d78792bb19d0284dfe37741c1fdeb4a516
Rasool Seyghaly <rasool.seyghaly@upc.edu>
2024-10-09 15:10:04 +0330

    0.1.6

--- bejoor/diophantine/diophantine_test.py
+++ bejoor/diophantine/diophantine_test.py
@@ -1,5 +1,5 @@
-from bejoor.diophantine.Diophantine import DiophantineEquation
-from bejoor.genetic import BaseGeneticAlgorithm
+from bejoor.diophantine import DiophantineEquation
+from bejoor.genetic import BaseGeneticAlgorithm,MemeticAlgorithm
 
 # Define a system of Diophantine equations
 equation_system = [
@@ -8,13 +8,13 @@ equation_system = [
 ]
 
 # Define bounds for the variables
-lower_bounds = [-50] * 3
-upper_bounds = [50] * 3
+lower_bounds = [-100] * 2
+upper_bounds = [100] * 2
 
 # Create a DiophantineEquation instance
-diophantine_eq = DiophantineEquation(equation_system, num_variables=3, lower_bound=lower_bounds, upper_bound=upper_bounds)
+diophantine_eq = DiophantineEquation(equation_system, num_variables=2, lower_bound=lower_bounds, upper_bound=upper_bounds)
 
-best_solution, best_objective_value = diophantine_eq.solve(BaseGeneticAlgorithm, population_size=300, epochs=200)
+best_solution, best_objective_value = diophantine_eq.solve(BaseGeneticAlgorithm, population_size=50, epochs=200)
 
 print("Best solution:", best_solution)
 print("Best objective value:", best_objective_value)
--- bejoor/economics_based/exchange_market_algorithm_test.py
+++ bejoor/economics_based/exchange_market_algorithm_test.py
@@ -1,13 +1,14 @@
 from bejoor.economics_based import ExchangeMarketAlgorithm
 
-def rosenbrock_function(sol):
-    return sum(100 * (sol[i+1] - sol[i]**2)**2 + (sol[i] - 1)**2 for i in range(len(sol) - 1))
+def sphere_function(sol):
+    return sum(x**2 for x in sol)
+
 
 solution_vector = [{"type": "float", "lower_bound": -5, "upper_bound": 10}] * 7
 
-ema = ExchangeMarketAlgorithm(objective_function=rosenbrock_function, solution_vector_size=7,
+ema = ExchangeMarketAlgorithm(objective_function=sphere_function, solution_vector_size=7,
                               solution_vector=solution_vector, optimization_side="min",
-                              trading_probability=0.8, population_size=50, epochs=50)
+                              trading_probability=0.8, population_size=250, epochs=50)
 ema.run()
 
 print(f'Best Global Objective Value: {ema.global_best_objective_value}')
